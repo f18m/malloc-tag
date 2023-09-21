d4d387b90bad85645e2cab0554eed9c0dd847496
Francesco Montorsi <fmontorsi@empirix.com>
2023-09-21 10:53:44 +0200

    f

--- tests/toomanylevels.cpp
+++ tests/toomanylevels.cpp
@@ -57,27 +57,23 @@ TEST(MallocTagTestsuite, TooManyLevels)
     Level1();
 
     MallocTagStatMap_t mtag_stats = MallocTagEngine::collect_stats();
-    for (const auto& it : mtag_stats)
-        std::cout << it.first << "=" << it.second << std::endl;
+
+    // decomment to debug this unit test:
+    // for (const auto& it : mtag_stats)
+    //    std::cout << it.first << "=" << it.second << std::endl;
 
     // check that malloc-tag has correctly handled the "too many tree levels" corner case
 
     // CHECK1: the malloc at level5 must end up accounted at level3 (last available level)
-    std::string key_for_inner_scope
-        = MallocTagEngine::get_stat_key_prefix_for_thread() + "toomanylevels.Level1.Level2.Level3";
-    EXPECT_EQ(mtag_stats[key_for_inner_scope + ".nCallsTo_malloc"], 1);
-    EXPECT_GE(mtag_stats[key_for_inner_scope + ".nBytesSelf"], MALLOC_AT_LEVEL5);
+    std::string k = MallocTagEngine::get_stat_key_prefix_for_thread() + "unit_tests.Level1.Level2.Level3";
+    EXPECT_EQ(mtag_stats[k + ".nCallsTo_malloc"], 1);
+    EXPECT_GE(mtag_stats[k + ".nBytesSelf"], MALLOC_AT_LEVEL5);
 
     // CHECK2: the malloc at level2 must end up correctly accounted at level2
     //         (this checks that all "pop" operations by failed MallocTagScope are correctly skipped)
-    key_for_inner_scope = MallocTagEngine::get_stat_key_prefix_for_thread() + "toomanylevels.Level1.Level2";
-    if (mtag_stats[key_for_inner_scope + ".nCallsTo_malloc"] == 1
-        && mtag_stats[key_for_inner_scope + ".nBytesSelf"] >= MALLOC_AT_LEVEL5)
-        std::cout << "SUCCESS: level3 accounted for the malloc at level5" << std::endl;
-    else {
-        std::cout << "FAILURE in level3 accounting" << std::endl;
-        ret_code = 1;
-    }
+    k = MallocTagEngine::get_stat_key_prefix_for_thread() + "unit_tests.Level1.Level2";
+    EXPECT_EQ(mtag_stats[k + ".nCallsTo_malloc"], 1);
+    EXPECT_GE(mtag_stats[k + ".nBytesSelf"], MALLOC_AT_LEVEL2);
 
     // dump stats in both JSON and graphviz formats
     if (MallocTagEngine::write_stats())
@@ -86,6 +82,4 @@ TEST(MallocTagTestsuite, TooManyLevels)
 
     // decomment this is you want to have the time to look at this process still running with e.g. "top"
     // sleep(100000);
-
-    std::cout << "Bye!" << std::endl;
 }
